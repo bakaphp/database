d7ca6c65916943fe59789b6e7266954b29d9e78d
kaioken <max@mctekk.com>
2018-12-24 17:00:13 -0400

    remove return type type from custom fields

--- src/Model.php
+++ src/Model.php
@@ -2,8 +2,6 @@
 
 namespace Baka\Database;
 
-use Phalcon\Mvc\Model\ResultsetInterface;
-
 class Model extends \Phalcon\Mvc\Model
 {
     /**
@@ -88,28 +86,6 @@ class Model extends \Phalcon\Mvc\Model
         $this->is_deleted = 1;
 
         return $this->save();
-    }
-
-    /**
-     * Allows to query a set of records that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return Content[]
-     */
-    public static function find($parameters = null)
-    {
-        return parent::find($parameters);
-    }
-
-    /**
-     * Allows to query the first record that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return Content
-     */
-    public static function findFirst($parameters = null)
-    {
-        return parent::findFirst($parameters);
     }
 
     /**
